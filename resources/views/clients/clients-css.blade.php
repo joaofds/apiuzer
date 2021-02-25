ebed62db0a2afa26d3f8e5c09e8a1ab42174bd32
João Ferreira <jo4o.fds@gmail.com>
2021-02-25 15:00:41 -0300

    stylesheet de clients views

---
+++ resources/views/clients/clients-css.blade.php
@@ -0,0 +1,37 @@
+<style type="text/css">
+    .container-fluid {
+        padding-left: 0;
+        padding-right: 0;
+    }
+
+    h1 {
+        color: #05b59d;
+        font-weight: 600;
+    }
+
+    p {
+        color: #028472;
+    }
+
+    i {
+        font-size: 5rem;
+        color: #b0b3b6;
+    }
+
+    i:hover {
+        color: #fff;
+    }
+
+    .btn {
+        background-color: #fff;
+    }
+
+    .btn:hover {
+        background-color: #028472;
+    }
+
+    .btn:focus,.btn:active {
+        outline: none !important;
+        box-shadow: none;
+    }
+</style>
