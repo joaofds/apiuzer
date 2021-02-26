da6b6ffe6708377ca04b8403a8853f2786f3cba6
João Ferreira <jo4o.fds@gmail.com>
2021-02-25 21:58:08 -0300

    listando registros

--- resources/views/clients/list.blade.php
+++ resources/views/clients/list.blade.php
@@ -12,33 +12,32 @@
             <div class="row">
                 <table class="table">
                     <thead class="bg-info">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Data/Criação</th>
+                            <th scope="col">Data/Atualização</th>
+                            <th scope="col">Ação</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
+                    @foreach($data as $cliente)
+                        <tr>
+                            <th scope="row">{{ $cliente['id'] }}</th>
+                        <td>{{ $cliente['nome'] }}</td>
+                        <td>{{ $cliente['email'] }}</td>
+                        <td>{{ $cliente['telefone'] }}</td>
+                        <td>{{ $cliente['created_at'] }}</td>
+                        <td>{{ $cliente['updated_at'] }}</td>
+                        <td>
+                            <a class="edit" id="edit" title="Edit" data-toggle="tooltip"><i class="bi bi-pencil-fill"></i></a>
+                            <a class="delete" id="delete"  title="Delete" data-toggle="tooltip"><i class="bi bi-trash"></i></a>
+                        </td>
                     </tr>
                     </tbody>
+                    @endforeach
                 </table>
             </div>
         </div>
